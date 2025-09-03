63e0f0477cb4891bc74e961a71f2b75132bea6a6
Nelly <nelsongael.diaz@gmail.com>
2025-09-03 12:56:54 -0400

    Working on adding a course assingmnets page and a courses page. Also reading documentation on adding some styling to the outputs in order to make the information more digestable.

---
+++ src/canvas_api.py
@@ -0,0 +1,52 @@
+import json
+from canvasapi import Canvas
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path=Path(__file__).resolve().parent / ".env")
+API_URL = os.getenv("CANVAS_API_URL", "https://webcourses.ucf.edu")
+API_KEY = os.getenv("CANVAS_API_KEY")
+
+if not API_KEY:
+    raise RuntimeError("Missing CANVAS_API_KEY. Create a .env file with CANVAS_API_KEY=... and ensure it's not committed.")
+
+BASE = API_URL.rstrip("/")
+canvas = Canvas(API_URL, API_KEY)
+
+# Sanity check
+def health():
+    return {
+        "ok" : True,
+        "api_url": API_URL,
+        "has_key": bool(API_KEY)
+    }
+
+# Display courses
+def list_courses():
+    return [
+        {
+            "courses_id": c.id,
+            "name": c.name,
+            "course_code": c.course_code,
+        }
+        for c in canvas.get_courses()
+    ]
+
+
+# Display Assignments
+def list_assignments(course_id):
+    course = canvas.get_course(course_id)
+
+    return [
+        {
+            "course_id": a.id,
+            "name": a.name,
+            "due_at": a.due_at,
+            "html_url": a.html_url
+        }
+
+        for a in course.get_assignments(order_by="due_at")
+        if a.due_at
+    ]
